b35edcf0834cb78c4cb0cd5e7dfed5a6410f96f1
sangnt <sangnt@seatechit.com.vn>
2023-11-18 14:49:58 +0700

    update autheticate

--- api/schema/authenticate.go
+++ api/schema/authenticate.go
@@ -1,10 +1,14 @@
 package schema
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	securityJWT "github.com/techschool/simplebank/api/security/jwt"
+	db "github.com/techschool/simplebank/db/sqlc"
+)
 
 // parseBearerAuth parses an HTTP Bearer Authenticate string
 // "Bearer 123hkladashdflhafkja.kbnlkabl return 123hkladashdflhafkja.kbnlkabl true"
-func ParseBearerAuth(ctx *gin.Context) (token string, ok bool) {
+func parseBearerAuth(ctx *gin.Context) (token string, ok bool) {
 	auth := ctx.Request.Header.Get("Authorization")
 	if auth == "" {
 		return "", false
@@ -15,4 +19,20 @@ func ParseBearerAuth(ctx *gin.Context) (token string, ok bool) {
 	}
 
 	return auth[len(prefix):], true
+}
+
+func GetUserByBeareToken(ctx *gin.Context, jwtMaker securityJWT.Maker, store db.Store) (string, bool) {
+	token, ok := parseBearerAuth(ctx)
+	if !ok {
+		return "", false
+	}
+	payload, err := jwtMaker.VerifyToken(token)
+	if err != nil {
+		return "", false
+	}
+	user, err := store.GetUser(ctx, payload.Username)
+	if err != nil {
+		return "", false
+	}
+	return user.Username, true
 }
